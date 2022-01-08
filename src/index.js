2fdea66778a2ad5eb321ff3089f7f886f2ad42b9
ed_bolma <ed.bolma21@gmail.com>
2022-01-08 22:53:47 +0100

    added list and coutnry data depends on what's in input box

--- src/index.js
+++ src/index.js
@@ -14,7 +14,35 @@ refs.button.addEventListener('click', onClickEvent);
 
 function onClickEvent(event) {
     // console.log(countryInput);
-    fetchCountry(countryInput);
+    fetchCountry(countryInput).then(data => {
+        console.dir(data)
+
+        console.log(data.length);
+
+        if (data.length > 1) {
+
+            console.log('more then 1')
+
+            const countryList = data.map(country => country.name.official);
+            console.log(countryList);
+
+            return
+        } 
+        
+        console.log('only one');
+
+        const countryData = {
+
+            officialName: data[0].name.official,
+            flag: data[0].flags.svg,
+            capital: data[0].capital,
+            population: data[0].population,
+            languages: data[0].languages,
+        }
+
+        console.log(countryData);
+
+    });; 
 }
 
 function onInput(event) {
@@ -22,8 +50,7 @@ function onInput(event) {
     countryInput = event.currentTarget.value
 }
 
-function fetchCountry(countryInput) {fetch(`https://restcountries.com/v3.1/name/${countryInput}?fields=name,capital,population,flags,languages;`)
-    .then(r => r.json()).then(data => {
-        console.dir(data[0])
-    });
+function fetchCountry(country) {
+     return fetch(`https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages;`)
+    .then(r => r.json());
 };
