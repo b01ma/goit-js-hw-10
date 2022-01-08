883e0be2a1b08c9d9d6bce852a65c9605a1e5cbf
ed_bolma <ed.bolma21@gmail.com>
2022-01-08 21:12:41 +0100

    begin of work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
 
+const refs = {
+    button: document.querySelector('.button-test'),
+    input: document.querySelector('#search-box'),
+}
+
 const DEBOUNCE_DELAY = 300;
+
+
+
+
+//  function fetchCountry(countryName) {fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages;`)
+//     .then(r => r.json()).then(data => {
+//         console.dir(data[0])
+//     });
+// };
+name(1);
+
+function name(x) {
+    console.log(`name is ${x}`)
+};
+
+
